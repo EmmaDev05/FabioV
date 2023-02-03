6f8717286169398e54b62c78ea33016fa5cbd0dd
Emmanuele Ballarani <86883649+EmmaDev05@users.noreply.github.com>
2023-02-03 21:51:29 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+
+/* Sidebar menu*/
+// close hamburger menu after click a
+$(".menu-list li a").on("click", function () {
+  $("#hamburger-menu").click();
+});
+
+$(".carousel-1").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+    1400: {
+      items: 4,
+    },
+    1600: {
+      items: 5,
+    },
+    2000: {
+      items: 6,
+    },
+  },
+});
